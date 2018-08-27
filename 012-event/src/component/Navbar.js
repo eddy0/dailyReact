3c20eaba75603313eeceebb84b262aaf1f715ed4
eddy0 <awindy369@gmail.com>
2018-08-26 23:46:13 -0700

    more file

---
+++ 012-event/src/component/Navbar.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+import {Menu} from 'semantic-ui-react'
+
+
+class Navbar extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Navbar
