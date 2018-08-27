e5f43e7673c73bb819207e62846865c1cd5290f7
eddy0 <awindy369@gmail.com>
2018-08-26 17:18:29 -0700

    add the commit

--- 012-event/src/reducer/index.js
+++ 012-event/src/reducer/index.js
@@ -1,12 +1,7 @@
 import {combineReducers} from 'redux'
-import {loadingBarReducer} from 'react-redux-loading-bar'
-import users from './user'
-import events from './event'
-import auth from './auth'
+
+
 
 export default combineReducers({
-    events,
-    users,
-    loadingBar: loadingBarReducer,
-    auth,
+
 })
